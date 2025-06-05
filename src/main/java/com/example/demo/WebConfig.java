b151fcca104446f617fc3f0e853f264f1bc89b57
Mitanshu surana <55337570+mitanshusurana@users.noreply.github.com>
2025-06-05 02:19:49 -0400

    Update WebConfig.java

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -14,10 +14,9 @@ public class WebConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*","http://localhost:4200","https://suranagemstest.netlify.app","https://backend-suranagems.onrender.com")
+                        .allowedOrigins("*") // Allow all origins
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
